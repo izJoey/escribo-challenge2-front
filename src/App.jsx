dec0c5fd864bd83839712d82972b2412216fdafc
Joey <joeyrjtd@gmail.com>
2023-11-23 15:54:50 -0300

    render changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./Router";
 
-function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
-    </>
-  );
-}
+ReactDOM.render(
+  <BrowserRouter>
+    <Router />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 export default App;
